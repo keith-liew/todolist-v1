a712d5829f7e6b19a96af6377900476b9d7aaf07
keith-liew <>
2022-09-23 22:06:39 +0800

    add enviroment port

--- app.js
+++ app.js
@@ -103,6 +103,6 @@ app.get("/:newParam", function (req, res) {
     })
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server stared on port 3000");
 });
